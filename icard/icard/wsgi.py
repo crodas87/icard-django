2ff74b65f07b79a35184b63ad959f0a8ae4b1e66
crodas <c16rodagomez@hotmail.com>
2023-07-01 02:28:16 -0400

    Modificando la ruta del setting del wsgi

--- icard/icard/wsgi.py
+++ icard/icard/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icard.icard.settings')
 
 application = get_wsgi_application()
